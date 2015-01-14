43153d64ef1df0d2d7f4728d73cb1a2541213b08
Ivan <topdollar.dt@gmail.com>
2015-01-14 10:02:26 +0200

    order list UI

--- RecognitionCore/src/main/java/com/mayer/recognition/fragment/history/HistoryListFragment.java
+++ RecognitionCore/src/main/java/com/mayer/recognition/fragment/history/HistoryListFragment.java
@@ -3,11 +3,15 @@ package com.mayer.recognition.fragment.history;
 import android.app.Fragment;
 import android.app.ListFragment;
 
+import com.mayer.recognition.R;
+
 import org.androidannotations.annotations.EFragment;
 
 /**
  * Created by dot on 19.11.2014.
  */
-@EFragment
+@EFragment(R.layout.fragment_order_list)
 public class HistoryListFragment extends ListFragment {
+
+
 }
